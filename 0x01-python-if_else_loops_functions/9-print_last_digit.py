7ed296c7f48fd5c9efeddae892ab624666f41bc7
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-05 12:51:41 +0100

    lets sees

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+def c_mod(a, b):
+    return a - b * int(a / b)
 def print_last_digit(number):
-    print("{}".format(number % 10), end="")
-    return (number % 10)
+    print("{}".format(c_mod(number, 10)), end="")
+    return (c_mod(number, 10))
