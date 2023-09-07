69ac88f1b49cf262db2db3f83e19158115a31c06
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-07 12:25:37 +0100

    added a check for the main attr

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) <= 1:
-    sys.exit()
-sum = 0
-for i in sys.argv[1:]:
-    sum += int(i)
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) <= 1:
+        sys.exit()
+    sum = 0
+    for i in sys.argv[1:]:
+        sum += int(i)
+    print("{}".format(sum))
