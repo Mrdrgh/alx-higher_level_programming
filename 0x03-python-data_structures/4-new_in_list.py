bc776f15c4ee30cccd24faea55221552f73cbaf1
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-08 17:32:59 +0100

    changed the copying method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,4 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1 or idx < 0:
         return (my_list.copy())
-    return (my_list.copy().insert(idx, element))
+    copy  = [i for i in my_list]
+    copy[idx] = element
+    return (copy)
