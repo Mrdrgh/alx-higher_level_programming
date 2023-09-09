d33f3ba20ae16c0fa6274a5719046ae5eae32ce1
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-09 11:54:45 +0100

    added the sixth task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for col in matrix:
+        for row in col:
+            print("{}".format(row), end=" " if row != col[-1] else"")
+        print()
+
