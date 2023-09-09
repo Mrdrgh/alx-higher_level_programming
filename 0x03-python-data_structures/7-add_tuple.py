69a254a756c2c212014f946785442d2a112a367c
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-09 12:14:04 +0100

    added edge casing

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     first_arg = 0
     second_arg = 0
-    if isinstance(tuple_a, tuple) and len(tuple_a) == 2:
-        first_arg += tuple_a[0]
-        second_arg += tuple_a[1]
-    if isinstance(tuple_b, tuple) and len(tuple_b) == 2:
-        first_arg += tuple_b[0]
-        second_arg += tuple_b[1]
+    if isinstance(tuple_a, tuple):
+        if len(tuple_a) > 0:
+            first_arg += tuple_a[0]
+        if len(tuple_a) > 1:
+            second_arg += tuple_a[1]
+    if isinstance(tuple_b, tuple):
+        if len(tuple_b) > 0:
+            first_arg += tuple_b[0]
+        if len(tuple_b) > 1:
+            second_arg += tuple_b[1]
     res = first_arg, second_arg
     return (res)
