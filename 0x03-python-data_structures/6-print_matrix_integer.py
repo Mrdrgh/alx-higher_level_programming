288424deaecb6985d0eee8aeafdfd71bafa143a5
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-09 11:57:29 +0100

    so guillame wants us to specify to a high level language what type of variable to output using print, whatev

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for col in matrix:
         for row in col:
-            print("{}".format(row), end=" " if row != col[-1] else"")
+            print("{:d}".format(row), end=" " if row != col[-1] else"")
         print()
 
