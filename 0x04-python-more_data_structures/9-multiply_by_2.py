8cdaccc0e653b7a49e57e321ffffdc2ae5877a56
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-13 22:39:11 +0100

    good to go

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = a_dictionary
-    for i in new_dic.values():
-        i **= 2
+    new_dic = a_dictionary.copy()
+    for i in list(new_dic):
+        new_dic[i] *= 2
     return (new_dic)
