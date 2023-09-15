64f9922997d9dc5f88261ed21374d07597793291
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-15 20:31:12 +0100

    should work now

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    l = list(a_dictionary)
-    max = l[1]
-    for i in l:
-        if i > max:
-            max = i
-    return (max)
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
+
  
