a310f6a6e0969394ece3ce8806b562a51c5042bd
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-25 18:21:10 +0100

    fixed some regs

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *args):
         res = fct(*args)
         return (res)
     except:
-        print("{}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
