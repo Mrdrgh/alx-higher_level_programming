6479a3e07cf8cdf5a8867843843761df2da618d1
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-26 21:27:50 +0100

    should this work ?

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define a Square class with size attribute
+that is private
+"""
+
+
+class Square:
+    """instantialte the square class
+    Args:
+        size (int): the size of the square
+    """
+    def __init__(self) -> None:
+        self.__size = size
