b59169bac814dc4edb9206f3a83ee58b8a952a5d
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-29 12:29:55 +0100

    first test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add the ints or the floats"""
+
+
+def add_integer(a, b=98):
+    sum = 0
+    try:
+        sum += a
+    except (TypeError):
+        raise TypeError("a must be an integer")
+    try:
+        sum += b
+    except (TypeError):
+        raise TypeError("b must be an integer")
+    return (sum)
