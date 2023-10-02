067b3ef66d66cf85330197384c98b3520ee7f4ab
Mrdrgh <mrdrgh2003@gmail.com>
2023-10-02 19:24:03 +0100

    fixed some refs

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -82,4 +82,4 @@ class Rectangle:
     def __del__(self):
         """de something when the object is guarbage collected"""
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        type(self).number_of_instances -= 1
