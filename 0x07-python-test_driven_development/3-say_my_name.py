2095dc115a65a4d14849e0cbd8985d25722bd4d4
Mrdrgh <mrdrgh2003@gmail.com>
2023-10-03 21:12:15 +0100

    pushed the last refs

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,11 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """print a name
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
