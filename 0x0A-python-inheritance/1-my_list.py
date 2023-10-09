b0c8981118c7bfa7589ba6498419203460db43bd
Mrdrgh <mrdrgh2003@gmail.com>
2023-10-09 20:23:46 +0100

    lets see

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""ok new file new chanllenge"""
+
+
+class Mylist(list):
+    """a class that enherits from a list
+    Args:
+        list (list): the list inherited"""
+    def print_sorted(self):
+        """print sorted this list"""
+        print("{}".format(sorted(list)))
