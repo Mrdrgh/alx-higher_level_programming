d392dc16440102b4b3b694c9c7662af06db09b6c
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-14 23:17:01 +0100

    changed the script by making atomic queries

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,35 +1,26 @@
--- create another table and add multiple rows
+-- create a table and add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (
     id INTEGER,
     name VARCHAR(256),
     score INTEGER
 );
-
-INSERT INTO second_table 
-(
-    id,
-    name,
-    score
-) VALUES
-(
-    (
-        1,
-        "John",
-        10
-    ),
-    (
-        2,
-        "Alex",
-        3
-    ),
-    (
-        3,
-        "Bob",
-        14
-    ),
-    (
-        4,
-        "George",
-        8
-    )
+INSERT INTO second_table (id, name, score) VALUES (
+    1,
+    "John",
+    10
+);
+INSERT INTO second_table (id, name, score) VALUES (
+    2,
+    "Alex",
+    3
+);
+INSERT INTO second_table (id, name, score) VALUES (
+    3,
+    "Bob",
+    14
+);
+INSERT INTO second_table (id, name, score) VALUES (
+    4,
+    "George",
+    8
 );
