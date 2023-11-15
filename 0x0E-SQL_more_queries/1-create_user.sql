548d584aa963d1af181e7286e784a3f595a37fde
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-15 19:47:32 +0100

    typo fixed

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create user and grant all privileges even if existing already
-CREATE USER IF NOT EXISTS user_0d_1@locathost IDENTIFIED BY
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY
 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
