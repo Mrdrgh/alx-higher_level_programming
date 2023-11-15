363b9acb7255124978cb7e157a711443966198bd
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-15 19:46:03 +0100

    what about now

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create user and grant all privileges even if existing already
-CREATE USER IF NOT EXISTS user_0d_1@locathost IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS user_0d_1@locathost IDENTIFIED BY
+'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
