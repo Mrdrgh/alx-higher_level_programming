c1f4640238f27271a4b1b6f1adbf1f23bc36df98
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-15 20:13:15 +0100

    bro i'm not looking at alldat

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,12 @@
 -- create a db and cities as table with multiple constrains in it
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INTEGER NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES state(id)
-);
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
