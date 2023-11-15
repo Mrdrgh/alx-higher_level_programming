e1f6b47c4f767e7d2ab31c3f1da07affe3cf2fc7
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-15 19:43:46 +0100

    create a user and grant him all the privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create user and grant all privileges even if existing already
+CREATE USER IF NOT EXISTS user_0d_1@locathost IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PREVILIGES ON *.* TO user_0d_1@localhost;
