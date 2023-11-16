6de4c83ded802831098f38f8acb154ca0790dc80
Mrdrgh <mrdrgh2003@gmail.com>
2023-11-16 18:49:34 +0100

    forgot a comma

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- list all the cities of california that can be found in the db
 SELECT id, name FROM cities
 WHERE state_id = (SELECT id from states WHERE name = "California")
-ORDER BY id ASC
+ORDER BY id ASC;
