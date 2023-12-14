25701a8802f6adf31f5771a9a79df4b5d5659f28
Mrdrgh <mrdrgh2003@gmail.com>
2023-12-14 22:16:23 +0100

    f it

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 function add(a, b) {
-	return (a + b);
+  return (a + b);
 }
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+if (process.argv[2] === undefined || process.argv[3] === undefined
+    || isNaN(process.argv[2]) || isNaN(process.argv[3])) {
+  console.log('invalid args');
+} else {
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+}
