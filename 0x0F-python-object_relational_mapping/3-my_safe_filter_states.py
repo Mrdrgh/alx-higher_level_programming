b24c18d7ba85ef5f041bab507deded158268ad95
Mrdrgh <mrdrgh2003@gmail.com>
2023-12-17 22:01:11 +0100

    .

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
+    match = sys.argv[4]
     cur.execute("SELECT * FROM states WHERE \
-            name = '{}' ORDER BY id ASC".format(sys.argv[4].split().join('')))
+            name = '%s'", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
