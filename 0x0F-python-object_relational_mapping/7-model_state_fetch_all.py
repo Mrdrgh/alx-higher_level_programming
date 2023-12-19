444ad7cf68bcd0107a4abb12ffd8e4f170f0b675
Mrdrgh <mrdrgh2003@gmail.com>
2023-12-19 19:07:32 +0100

    push all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """whatever"""
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
 import sys
