0c1f8ae6dc9e9b30136a093cd70ef80a4d782a9a
Mrdrgh <mrdrgh2003@gmail.com>
2023-12-20 00:01:37 +0100

    what about now

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
                                    sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    instace = session.query(State).filter(State.
-                                          name.like("{}".
-                                                    format(sys.
-                                                           argv[4])))
-    for inst in instace:
-        print(inst.id, inst.name, sep=': ')
+    inst = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(inst[0].id)
+    except IndexError:
+        print("Not found")
